767ef53f751fba9ee511c70cf49b73bc02ce0c1f
root <root@ptorch-rl-vm.us-west1-b.c.aj-ptorch-rl.internal>
2020-10-28 03:28:27 +0000

    New Plot

--- Friction_Finger_Gripper_DQN/main.py
+++ Friction_Finger_Gripper_DQN/main.py
@@ -622,7 +622,7 @@ if __name__ == '__main__':
         actions=[]
         start, goal = env.reset()
         start = (7.9, 7.2, -90)
-        goal = (9.9, 7.2, -90)
+        goal = (9.9, 7.2, 0)
         c_state = np.concatenate((np.array(start), np.array(goal)))
 
         env.current_state = start
@@ -637,7 +637,7 @@ if __name__ == '__main__':
 
         n=0
         
-        while(not done and n<150):
+        while(not done and n<250):
 
             n =n+1
             # state = torch.from_numpy(c_state).float().unsqueeze(0)
